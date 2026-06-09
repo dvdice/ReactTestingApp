bd3560f7a5e8b0a57b6bee09ddd7c353405bb761
dvdice <valitovdamir73@gmail.com>
2026-06-09 21:32:21 +0400

    Добавил таймер - 10 сек для ответа на вопрос

--- testing-app/src/components/App.tsx
+++ testing-app/src/components/App.tsx
@@ -11,6 +11,7 @@ function App() {
     const [isLoading, setIsLoading] = useState(true);
     const [questions, setQuestions] = useState<Question[]>([]);
     const [difficulty, setDifficulty] = useState<string>('easy');
+    const [timeLeft, setTimeLeft] = useState(10);
 
     async function fetchData() {
         setIsLoading(true);
@@ -40,9 +41,23 @@ function App() {
 
     useEffect(() => {
         fetchData()
+        setTimeLeft(10)
     }, [difficulty]);
 
-    const handleAnswerButtonClick = (selectedAnswer: string) => {
+    useEffect(() => {
+        const id = setInterval(() => {
+            if (timeLeft === 0)
+                handleAnswerButtonClick('')
+            else
+                setTimeLeft((prevState) => prevState - 1);
+
+        }, 1000)
+        return () => {
+            clearInterval(id);
+        }
+    }, [currentQuestion, timeLeft]);
+
+    function handleAnswerButtonClick(selectedAnswer: string) {
         if (selectedAnswer === questions[currentQuestion].correctAnswer)
             setScore(score + 1)
 
@@ -50,13 +65,15 @@ function App() {
             setCurrentQuestion(currentQuestion + 1);
         else
             setIsFinished(true);
+
+        setTimeLeft(10);
     }
 
     const restartTest= async () => {
         await fetchData();
-        setCurrentQuestion(0);
         setIsFinished(false);
         setScore(0);
+        setTimeLeft(10);
     }
 
     if (isFinished) {
@@ -76,6 +93,7 @@ function App() {
             return (
                 <div>
                     <h1>Приложение для тестирования. Вопрос {currentQuestion + 1} из {questions.length}</h1>
+                    <h3>Времени осталось {timeLeft}</h3>
 
                     <select value={difficulty} onChange={(e) => setDifficulty(e.target.value)}>
                         <option value="easy">Легко</option>
